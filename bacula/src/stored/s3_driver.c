cdfb3d6b64c3819a26ec6e20202adec0d80a87cb
firebladed <34522909+firebladed@users.noreply.github.com>
2021-10-13 18:38:14 +0100

    Update s3_driver.c

--- bacula/src/stored/s3_driver.c
+++ bacula/src/stored/s3_driver.c
@@ -462,7 +462,7 @@ S3Status s3_driver::put_object(transfer *xfer, const char *cache_fname, const ch
         0,                                       // contentDispositionFilename
         0,                                       // contentEncoding
        -1,                                       // expires
-        cannedAcl,                               // cannedAcl
+        S3CannedAclPrivate,                      // cannedAcl
         0,                                       // metaDataCount
         0,                                       // metaData
         0                                        // useServerSideEncryption
